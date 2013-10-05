c9619714a4485b5ce2b44607cf533749a473fef7
circuschaser <rob@circuschaser.com>
2013-10-05 19:18:54 -0400

    welcome message on new user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,7 +19,7 @@ class UsersController < ApplicationController
   	@user = User.new(params[:user])
   	if @user.save
       sign_in @user
-      flash[:success] = "Welcome to AmateurMorpheus"
+      flash[:success] = "Welcome to the Promorpheus PROTOTYPE"
   		redirect_to @user
   	else
   		render 'new'
