4e8f7bb6706c0f97c9301802b5c0f1a57fad9f35
circuschaser <rob@circuschaser.com>
2013-10-15 14:44:26 -0400

    mod db:seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,20 +3,20 @@
 
 
 
-# Composer.delete_all
+Composer.delete_all
 # CSV.foreach("doc/Composers.csv", headers: true) do |row|
 # 	Composer.create! row.to_hash
 # end
 
 
-# Album.delete_all
+Album.delete_all
 # CSV.foreach("doc/Albums.csv", headers: true) do |row|	
 # 	c = Composer.find_by_name row["composer_name"]	
 # 	a = c.albums.create! row.to_hash
 # end
 
 
-# Genre.delete_all
+Genre.delete_all
 # CSV.foreach("doc/Genres.csv", headers: true) do |row|
 # 	Genre.create! row.to_hash
 # end
@@ -30,17 +30,17 @@ CSV.foreach("doc/Songs.csv", headers: true) do |row|
 	s.update_attribute(:composer_id, (Composer.find_by_name s.composer_name).id)
 end
 
-songs = Song.all
-songs.each do |s|
-	s.update_attribute(:tag_list, [])
-end
+# songs = Song.all
+# songs.each do |s|
+# 	s.update_attribute(:tag_list, [])
+# end
 
-3.times do
-	CSV.foreach("doc/Tags.csv", headers: false) do |row|
-		s = Song.all.shuffle.pop
-		s.update_attribute(:tag_list, row)
-	end
-end
+# 3.times do
+# 	CSV.foreach("doc/Tags.csv", headers: false) do |row|
+# 		s = Song.all.shuffle.pop
+# 		s.update_attribute(:tag_list, row)
+# 	end
+# end
 
 # 1000.times do
 #   Person.create :name => "#{Faker::Name.first_name} #{Faker::Name.last_name}"
