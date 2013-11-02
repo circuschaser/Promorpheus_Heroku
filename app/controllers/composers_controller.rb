09c6e5568a975929e54fb1e24667bdbae2724f3e
circuschaser <rob@circuschaser.com>
2013-11-02 17:49:53 -0400

    autocomplete

--- app/controllers/composers_controller.rb
+++ app/controllers/composers_controller.rb
@@ -3,9 +3,16 @@ class ComposersController < ApplicationController
   def show
     @composer = Composer.find(params[:id])
     # @selected_composer = Composer.find(params[:id])
-
     @songs = @composer.songs.paginate(per_page: 10, page: params[:page])
+  end
 
+  def index
+    @composers = Composer.search(params[:search]).paginate(per_page: 10, page: params[:page])
+    
+    respond_to do |format|  
+      format.html # index.html.erb  
+      format.json { render json: @composers }
+    end
   end
 
 end
